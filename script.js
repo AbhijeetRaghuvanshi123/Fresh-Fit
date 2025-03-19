50cc6db5baf490695b0a671b0b98f524501f857c
AbhijeetRaghuvanshi123 <abhijeetraghuvanshi9842abhi@gmail.com>
2025-03-19 10:21:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
+menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
 
-window.onscroll = () => {
+window.onscroll = () =>{
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
 
-//Text animation code
 
-const typed = new Typed('.multiple-text', {
-    strings: ['Physical Fitness', 'Weight Gain', 'Fat Loss', 'Muscle Building', 'Weight Lifting','Endurance Training'],
-    typeSpeed: 60,
-    backSpeed: 60,
-    backDelay: 1000,
-    loop: true
-});  
 
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const serviceRows = document.querySelectorAll('.row');
+
+    serviceRows.forEach(row => {
+        row.addEventListener('click', () => {
+            // Close any other open descriptions
+            serviceRows.forEach(otherRow => {
+                if (otherRow !== row && otherRow.classList.contains('active')) {
+                    otherRow.classList.remove('active');
+                }
+            });
+
+            // Toggle the active class on the clicked row
+            row.classList.toggle('active');
+        });
+    });
+});
